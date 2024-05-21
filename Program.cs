02c5d669ccc0257c10d2c861a0a2477455df2d27
Massoto10 <109998416+Massoto10@users.noreply.github.com>
2024-05-21 10:39:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Teste Nokia");
+Smartphone nokia = new Nokia(numero: "21998107410", modelo: "Moto G Force", imei: "093451233", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Teste Iphone");
+Smartphone iphone = new Iphone(numero: "21970758285", modelo: "13 ProMAx", imei: "321542871", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
